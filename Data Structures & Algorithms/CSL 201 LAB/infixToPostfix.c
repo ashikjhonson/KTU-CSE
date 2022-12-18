2632b3f30bee8513280185d8444cfaf625fec13c
Ashik Jhonson <97791612+ashikjhonson@users.noreply.github.com>
2022-12-18 11:57:03 +0530

    Update infixToPostfix.c

--- Data Structures & Algorithms/CSL 201 LAB/infixToPostfix.c
+++ Data Structures & Algorithms/CSL 201 LAB/infixToPostfix.c
@@ -1,58 +1,44 @@
 #include <stdio.h>
-#include <string.h>
 #include <ctype.h>
 
-char str[15], stack[15], a, s='$', x;
-int top = -1, size = 15;
-
-void push(char c)
-{
-    if(top==size-1)
-        return;
-    else
-        stack[++top] = c;
-}
+char stack[20], in[20],*e, x, a;
+int top = -1, size = 20;
 
 int priority(char c)
 {
-    if(c=='+'||c=='-')
-        return 1;
-    if(c=='*'||c=='/')
-        return 2;
-    if(c=='^')
-        return 3;
+    if(c=='+'||c=='-')      return 1;
+    if(c=='*'||c=='/')      return 2;
+    if(c=='^')              return 3;
     return 0;
+}
+
+void push(char c)
+{
+    if(top==size-1)     return ;
+    else                stack[++top] = c;
 }
 
 char pop()
 {
-    if(top!=-1)
-        return stack[top--];
-    else
-        return '0';
+    if(top>-1)      return stack[top--];
+    else            return '0';
 }
 
 void main()
 {
-    printf("Enter an expression: ");
-    fgets(str,15,stdin);
-    char *e = str;
+    fgets(in,20,stdin);
     push('$');
+    e = in;
     while(*e!='\0')
-    {   
+    {
         a = *e;
+        if(isalnum(a))      printf("%c",a);
 
-        if(isalnum(a))
-            printf("%c",a);
+        else if(a=='(')     push(a);
 
-        else if(a=='(')
-            push(a);
-        
-        else if (a==')')
-        {
-            while((x=pop())!='(')
-                printf("%c",x);
-        }
+        else if(a==')')     
+            while((x=pop())!='(')   printf("%c",x);
+
         else
         {
             while(priority(stack[top])>priority(a))
@@ -61,6 +47,5 @@ void main()
         }
     e++;
     }
-    while((x=pop())!='$')
-        printf("%c",x);
+    while((x=pop())!='$')       printf("%c",x);
 }
