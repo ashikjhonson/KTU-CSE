103200875a1ce08c224e6c935436065d7658c443
Ashik Jhonson <ashikjhonson@gmail.com>
2023-02-21 05:41:49 +0530

    updates

---
+++ S3/CSL 201 DATA STRUCTURES LAB/Model Questions/Graph Traversal/dfs.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+}*graph[20] = {NULL}, *new, *curr;
+
+void addEdge(struct node* graph[], int u, int v)
+{
+    new = malloc(sizeof(struct node));
+    new->data = v;
+    new->next = graph[u];
+    graph[u] = new;
+}
+
+void dfs(struct node* graph[], int v, int visited[])
+{
+    visited[v] = 1;
+    printf("%d ",v);
+
+    curr = graph[v];
+    while(curr)
+    {
+        int u = curr->data;
+        if(!visited[u])
+        {
+            dfs(graph, u, visited);
+        }
+        if(curr)
+            curr = curr->next;
+    }
+}
+
+void main()
+{
+    int n, e;
+    scanf("%d%d",&n,&e);
+
+    for(int i=0; i<e; i++)
+    {
+        int u, v;
+        scanf("%d%d",&u,&v);
+        addEdge(graph, u, v);
+        addEdge(graph, v, u);
+    }
+
+    int visited[20] = {0};
+    dfs(graph, 0, visited);
+}
--- S3/CSL 201 DATA STRUCTURES LAB/Model Questions/Sorting/quicksort.c
+++ S3/CSL 201 DATA STRUCTURES LAB/Model Questions/Sorting/quicksort.c
@@ -2,24 +2,26 @@
 
 int partition(int arr[], int l, int h)
 {
-    int pivot = arr[h], partitionInd = l;
+    int pivot = arr[h];
+    int pIndex = l;
 
     for(int i=l; i<h; i++)
     {
         if(arr[i]<=pivot)
         {
             int temp = arr[i];
-            arr[i] = arr[partitionInd];
-            arr[partitionInd] = temp;
+            arr[i] = arr[pIndex];
+            arr[pIndex] = temp;
 
-            partitionInd++;
+            pIndex++;
         }
     }
-    int temp = arr[h];
-    arr[h] = arr[partitionInd];
-    arr[partitionInd] = temp;
 
-    return partitionInd;
+    int temp = arr[h];
+    arr[h] = arr[pIndex];
+    arr[pIndex] = temp;
+
+    return pIndex;
 }
 
 void quicksort(int arr[], int l, int h)
@@ -34,7 +36,7 @@ void quicksort(int arr[], int l, int h)
 
 void main()
 {
-    int arr[] = {0,2,5,1,4};
+    int arr[] = {0,2,3,1,4};
     printf("Array: ");
     for(int i=0; i<5; i++)
         printf("%d ",arr[i]);
